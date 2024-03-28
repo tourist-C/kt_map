56d1fb35032c8fa8a8b972e57fa9b46f9f415fba
Curtis <ckto.curtis@gmail.com>
2024-03-28 18:17:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,25 +14,27 @@ import streamlit as st
 # )
 
 # Add custom CSS to hide the GitHub icon
-# hide_github_icon = """
-# #GithubIcon {
-#   visibility: hidden;
-# }
-# """
-# st.markdown(hide_github_icon, unsafe_allow_html=True)
+hide_github_icon = """
+#stActionButtonIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+# <div data-testid="stActionButtonIcon" class="st-emotion-cache-q16mip e3g6aar1"></div>
+# st.markdown(
+#     """
+#     <style>
+#     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+#     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+#     .viewerBadge_text__1JaDK {
+#         display: none;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 
 st.write("!2312")
